92f57d4807ea6bf2a9f9019b33c202a872c55050
rr222cy <rr222cy@student.lnu.se>
2014-10-01 11:30:52 +0200

    UserView updated with structure

--- BoatClub/BoatClub/view/UserView.cs
+++ BoatClub/BoatClub/view/UserView.cs
@@ -8,5 +8,58 @@ namespace BoatClub.view
 {
     class UserView
     {
+        // Fields
+
+        // Properties
+
+        // Methods
+
+        private static void ContinueOnKeyPressed()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nTryck på en tangent för att fortsätta!\n");
+            Console.ResetColor();
+            Console.CursorVisible = false;
+            Console.ReadKey(true);
+            Console.Clear();
+            Console.CursorVisible = true;
+        }
+
+        public void AddUser()
+        {
+
+        }
+
+        public void UpdateUser()
+        {
+
+        }
+
+        public void RemoveUser()
+        {
+            
+        }
+
+        public void ShowUser()
+        {
+            
+        }
+
+        public void ShowUsersSimple()
+        {
+
+        }
+
+        public void ShowUsersFull()
+        {
+
+        }
+
+        // Constructor
+        public UserView()
+        {
+            // TOM!
+        }
     }
 }
