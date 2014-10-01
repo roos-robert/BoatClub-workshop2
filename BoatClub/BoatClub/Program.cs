fb5f9008e9201d1308859764b235ed3038fafd96
rr222cy <rr222cy@student.lnu.se>
2014-10-01 10:37:30 +0200

    Connection of layers in Program.cs

--- BoatClub/BoatClub/Program.cs
+++ BoatClub/BoatClub/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BoatClub.model;
+using BoatClub.view;
 
 namespace BoatClub
 {
@@ -10,7 +12,10 @@ namespace BoatClub
     {
         static void Main(string[] args)
         {
+            User u = new User();
+            Boat b = new Boat();
 
+            AppView v = new AppView();
         }
     }
 }
