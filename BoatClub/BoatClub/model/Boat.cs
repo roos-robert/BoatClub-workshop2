0f90812517f4a9e1f25e4f18b5d4d472f93965a9
mk222xa <mk222xa@student.lnu.se>
2014-10-01 10:18:18 +0200

    Boatclass updated
    BOATS!

--- BoatClub/BoatClub/model/Boat.cs
+++ BoatClub/BoatClub/model/Boat.cs
@@ -8,9 +8,55 @@ namespace BoatClub.model
 {
     class Boat
     {
-        private int BoatID;
-        private int MemberID;
-        private string Type;
-        private int Length;
+        //fields
+        private int _BoatID;
+        private int _MemberID;
+        private string _BoatType;
+        private int _Length;
+
+        //properties
+        public int BoatId 
+        {
+            get { return this._BoatID; }
+            set { this._BoatID = value; }
+        }
+
+        public int MemberID
+        {
+            get { return this._MemberID; }
+            set { this._MemberID = value; }
+        }
+        public string BoatType
+        {
+            get { return this._BoatType; }
+            set { this._BoatType = value; }
+        }
+
+        public int Length
+        {
+            get { return this._Length; }
+            set { this._Length = value; }
+        }
+
+        //Methods
+        public void AddBoat() 
+        {
+            
+        }
+
+        public void RemoveBoat()
+        {
+
+        }
+
+        public void UpdateBoat()
+        {
+
+        }
+
+        public void GetBoatInfo()
+        {
+            throw new NotImplementedException("Finns ej");
+        }
     }
 }
