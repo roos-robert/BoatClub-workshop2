1851b2b81f5d6c034ff3269d1ccd7b15b5ae612f
rr222cy <rr222cy@student.lnu.se>
2014-10-01 10:16:39 +0200

    Structure for User.cs added

--- BoatClub/BoatClub/model/User.cs
+++ BoatClub/BoatClub/model/User.cs
@@ -8,5 +8,67 @@ namespace BoatClub.model
 {
     class User
     {
+        // Fields
+        private int memberID;
+        private string name;
+        private int socialSecurity;
+
+        // Properties
+        public int MemberID
+        {
+            get { return this.memberID; }
+            set { this.memberID = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int SocialSecurity
+        {
+            get { return this.socialSecurity; }
+            set { this.socialSecurity = value; }
+        }
+
+        // Methods
+        public void AddUser()
+        {
+
+        }
+
+        public void RemoveUser()
+        {
+
+        }
+
+        public void UpdateUser()
+        {
+
+        }
+
+        public User ShowUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User ShowUsersSimple()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User ShowUsersFull()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Constructor
+
+        public User()
+        {
+            // TOM!
+        }
     }
 }
+    
